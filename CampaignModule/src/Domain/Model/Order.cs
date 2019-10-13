d488381cdb3f1615eb6be6eb5310cbec5b24ea01
Ferhat Candaş <candasferhat61@gmail.com>
2019-10-13 03:02:27 +0300

    entity models added

---
+++ CampaignModule/src/Domain/Model/Order.cs
@@ -0,0 +1,50 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Model
+{
+    public class Order : BaseEntity, IEntity
+    {
+        public static List<Order> OrderList { get; private set; }
+        public Quantity Quantity { get; private set; }
+        public Product OrderedProduct { get; private set; }
+        public Order()
+        {
+            OrderList = new List<Order>();
+        }
+        private Order(Product product, int quantity)
+        {
+            OrderedProduct = product;
+            Quantity = new Quantity(quantity);
+        }
+        public void AddOrder(string productCode, int quantity)
+        {
+            //get exist product from productList
+            var existProduct = Product.GetProdcut(productCode);
+            //get exist order
+            var existOrder = Order.GetOrder(productCode);
+            //decrase stock count
+            existProduct.Stock.DecraseStock(quantity);
+            if (existOrder == null)
+            {
+                //add product to order
+                OrderList.Add(new Order(existProduct, quantity));
+            }
+        }
+        public static Order GetOrder(string productCode)
+        {
+            var order = OrderList.FirstOrDefault(x => x.OrderedProduct.ProductCode.Value == productCode);
+            if (order == null)
+            {
+                return order;
+            }
+            else
+            {
+                throw new LogicException("This product has ordered");
+            }
+        }
+    }
+}
---
+++ CampaignModule/src/Domain/Model/Product.cs
@@ -0,0 +1,34 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Model
+{
+    public class Product : BaseEntity, IEntity
+    {
+        public static List<Product> ProductList { get; private set; }
+        public Product()
+        {
+            ProductList = new List<Product>();
+        }
+        public ProductCode ProductCode { get; private set; }
+        public Price Price { get; private set; }
+        public Stock Stock { get; private set; }
+      
+        public static Product GetProdcut(string productCode)
+        {
+            var product = ProductList.FirstOrDefault(x => x.ProductCode.Value == productCode);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new LogicException("Product not exits");
+            }
+        }
+
+    }
+}
