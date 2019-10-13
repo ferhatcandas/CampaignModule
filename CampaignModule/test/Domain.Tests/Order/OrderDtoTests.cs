513bded1a3d8c5b4019d4575c2fbff2c12144174
Ferhat Candaş <candasferhat61@gmail.com>
2019-10-13 20:53:31 +0300

    tests

---
+++ CampaignModule/test/Domain.Tests/Order/OrderDtoTests.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Order;
+using Domain.Product;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Domain.Tests.Order
+{
+    public class OrderDtoTests
+    {
+        [Fact]
+        public void Order_Should_SalesPrice_Set()
+        {
+            var order = GetMockData();
+
+            order.SetSalesPrice(50);
+            order.SalesPrice.Value.Should().Be(50);
+        }
+        public OrderDto GetMockData()
+        {
+            var product = new ProductDto("p1", 100, 1000);
+            return new OrderDto(product, 4);
+        }
+    }
+}
---
+++ CampaignModule/test/Domain.Tests/Product/ProductDtoTests.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Campaign;
+using Domain.Product;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Domain.Tests.Product
+{
+    public class ProductDtoTests
+    {
+        [Fact]
+        public void Product_Should_HasStock_ReturnFalse()
+        {
+            var product = GetMockData();
+            product.HasStock(0).Should().BeTrue();
+            product.HasStock(1001).Should().BeFalse();
+        }
+        [Fact]
+        public void Product_Should_MakeDiscount_IsSuccess()
+        {
+            var product = GetMockData();
+            product.Price.Value.Should().Be(100);
+            product.SetCampaign(new CampaignDto("c1", product, 10, 10, 980));
+            product.MakeDiscount(-5);
+            product.Price.Value.Should().Be(95);
+        }
+        [Fact]
+        public void Product_Should_MakeDiscount_IsFail()
+        {
+            var product = GetMockData();
+            product.Price.Value.Should().Be(100);
+            product.SetCampaign(new CampaignDto("c1", product, 10, 10, 980));
+            product.MakeDiscount(-80);
+            product.Price.Value.Should().Be(100);
+        }
+        private ProductDto GetMockData()
+        {
+            var product = new ProductDto("p1", 100, 1000);
+            return product;
+        }
+    }
+}
