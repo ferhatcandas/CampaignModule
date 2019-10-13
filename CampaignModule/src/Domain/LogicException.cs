c32e469524b79936c63f6018230ce3e2cae4d429
Ferhat Candaş <candasferhat61@gmail.com>
2019-10-13 03:02:47 +0300

    Create LogicException.cs

---
+++ CampaignModule/src/Domain/LogicException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class LogicException : Exception
+    {
+        public LogicException(string message) : base(message)
+        {
+
+        }
+    }
+}
